331d1d07b50747144cd174a5d7a767b45210cb36
alcides Paz <alcidesnp@hotmail.com>
2023-09-29 21:51:02 -0300

    Exercicios pendentes

---
+++ Atividade01/src/Atv02.java
@@ -0,0 +1,21 @@
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Atv02 {
+    public static void main(String[] args) {
+
+        Scanner password = new Scanner(System.in);
+        String senha;
+        do {
+            System.out.println("Digite a senha: ");
+
+            senha = password.nextLine();
+            if (!Objects.equals(senha, "123")) {
+                System.out.println("erro");
+            }
+        } while (!Objects.equals(senha, "123")); {
+            System.out.println("ok");
+        }
+    }
+}
+
