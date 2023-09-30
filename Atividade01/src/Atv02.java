57c4a75f2125a341b062f7509c3403d01a1a0703
alcides Paz <alcidesnp@hotmail.com>
2023-09-29 21:54:05 -0300

    Correção

--- Atividade01/src/Atv02.java
+++ Atividade01/src/Atv02.java
@@ -13,9 +13,11 @@ public class Atv02 {
             if (!Objects.equals(senha, "123")) {
                 System.out.println("erro");
             }
-        } while (!Objects.equals(senha, "123")); {
+        } while (!Objects.equals(senha, "123"));
+//        Acima, o while não precisa de ação por se tratar de condição para sair. Ao sair, seguirá a linha abaixo
+        
+
             System.out.println("ok");
-        }
     }
 }
 
